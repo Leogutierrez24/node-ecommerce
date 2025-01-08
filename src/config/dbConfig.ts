2db67deee99dcb1bf3c6af50f5d118883e03346d
Leogutierrez24 <leoalegutierrez97@gmail.com>
2025-01-07 23:25:33 -0300

    Fixed the connection problem.

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -8,5 +8,5 @@ export const dbConfig: ProcessEnv = {
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.PORT || "3000",
+  DB_PORT: process.env.DB_PORT || "3000",
 }
